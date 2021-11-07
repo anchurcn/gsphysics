94519bf3d288a49699d9074757ff422df33322ba
Anchur <809595156@qq.com>
2021-11-07 23:28:35 +0800

    move studio renderer export-related to HLSDK

--- src/PhyStudioModelRenderer.cpp
+++ src/PhyStudioModelRenderer.cpp
@@ -75,8 +75,6 @@ extern "C" {
 // Global engine <-> studio model rendering code interface
 extern engine_studio_api_t IEngineStudio;
 
-// The renderer object, created on the stack.
-PhyStudioModelRenderer g_StudioRenderer;
 /*
 ====================
 PhyStudioModelRenderer
@@ -431,76 +429,3 @@ int PhyStudioModelRenderer::StudioDrawPlayer(int flags, entity_state_s* pplayer)
 
 	return CStudioModelRenderer::StudioDrawPlayer(flags, pplayer);
 }
-
-////////////////////////////////////
-// Hooks to class implementation
-////////////////////////////////////
-
-/*
-====================
-R_StudioDrawPlayer
-
-====================
-*/
-int R_StudioDrawPlayer(int flags, entity_state_t* pplayer)
-{
-	return g_StudioRenderer.StudioDrawPlayer(flags, pplayer);
-}
-
-/*
-====================
-R_StudioDrawModel
-
-====================
-*/
-int R_StudioDrawModel(int flags)
-{
-	return g_StudioRenderer.StudioDrawModel(flags);
-}
-
-/*
-====================
-R_StudioInit
-
-====================
-*/
-void R_StudioInit(void)
-{
-	g_StudioRenderer.Init();
-}
-
-// The simple drawing interface we'll pass back to the engine
-r_studio_interface_t studio =
-{
-	STUDIO_INTERFACE_VERSION,
-	R_StudioDrawModel,
-	R_StudioDrawPlayer,
-};
-
-/*
-====================
-HUD_GetStudioModelInterface
-
-Export this function for the engine to use the studio renderer class to render objects.
-====================
-*/
-int DLLEXPORT HUD_GetStudioModelInterface(int version, struct r_studio_interface_s** ppinterface, struct engine_studio_api_s* pstudio)
-{
-	//	RecClStudioInterface(version, ppinterface, pstudio);
-
-	if (version != STUDIO_INTERFACE_VERSION)
-		return 0;
-
-	// Point the engine to our callbacks
-	*ppinterface = &studio;
-
-	// Copy in engine helper functions
-	memcpy(&IEngineStudio, pstudio, sizeof(IEngineStudio));
-
-	// Initialize local variables, etc.
-	R_StudioInit();
-
-	// Success
-	return 1;
-}
-
